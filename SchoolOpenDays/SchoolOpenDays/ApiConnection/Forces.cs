d21286c929b168ef2ef86f4e18d8e38c76107f1c
xRalph18 <rafal.machnik4@gmail.com>
2023-03-28 19:50:55 +0200

    UKPD Forces Api Connection

---
+++ SchoolOpenDays/SchoolOpenDays/ApiConnection/Forces.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace SchoolOpenDays.ApiConnection
+{
+    class Forces
+    {
+        public static List<Forces> GetForces()
+        {
+            HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format("https://data.police.uk/api/forces"));
+
+            WebReq.Method = "GET";
+
+            HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
+
+            Console.WriteLine(WebResp.StatusCode);
+            Console.WriteLine(WebResp.Server);
+
+            string jsonString;
+            using(Stream stream = WebResp.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+                jsonString = reader.ReadToEnd();
+            }
+
+            List<Forces> forces = JsonConvert.DeserializeObject<List<Forces>>(jsonString);
+
+            return forces;
+        }
+
+
+    }
+}
---
+++ SchoolOpenDays/SchoolOpenDays/Models/ForcesModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolOpenDays.Models
+{
+    class ForcesModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
