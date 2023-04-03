46d8391f3a51c2895eaed2b4c2dd2c9e7b4e08cd
xRalph18 <rafal.machnik4@gmail.com>
2023-04-03 15:44:15 +0200

    search bar

--- SchoolOpenDays/SchoolOpenDays/MainPage.xaml.cs
+++ SchoolOpenDays/SchoolOpenDays/MainPage.xaml.cs
@@ -9,24 +9,31 @@ using SchoolOpenDays.ApiConnection;
 using SchoolOpenDays.Models;
 using System.Windows.Input;
 using Xamarin.Essentials;
+using System.Collections;
 
 namespace SchoolOpenDays
 {
     public partial class MainPage : ContentPage
     {
+        private List<ForceDetailModel> ForcesList { get; set; }
         public MainPage()
         {
             InitializeComponent();
 
             List<ForcesModel> forces = Forces.GetForces();
-            List<ForceDetailModel> detailedForces = new List<ForceDetailModel>();
+            ForcesList = new List<ForceDetailModel>();
 
             foreach (var item in forces)
             {
-                detailedForces.Add(Forces.GetForceDetail($"{item.id}"));
+                ForcesList.Add(Forces.GetForceDetail($"{item.id}"));
             }
 
-            listView.ItemsSource = detailedForces;
+            listView.ItemsSource = ForcesList;
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            listView.ItemsSource = ForcesList.Where(s => s.name.ToLower().Replace(" ", "").Contains(e.NewTextValue.ToLower().Replace(" ", "")));
         }
     }
 }
